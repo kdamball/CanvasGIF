e6383645adf3e3350486f07f144e961415f94d4d
Kado Damball <kadodamball@hotmail.com>
2014-05-18 13:06:10 +0300

    IIFE vs Inline event binding fix

--- js/gifer.js
+++ js/gifer.js
@@ -1,10 +1,13 @@
-$(function(){
+(function(){
   var pics = ['tree1','tree2','tree3','tree4','tree5','tree6','tree7','tree8'];
   var cvs = document.getElementById('svc');
   var c = cvs.getContext('2d');
   var i = 0;
   var x = pics.length;
-  var forward, moonwalk;
+  var forward, moonwalk,
+    front = document.getElementById("front"),
+    back = document.getElementById("back"),
+    pause = document.getElementById("pause");
   function moveb(){
     stopIt();
     moonwalk = setInterval(function(){
@@ -30,5 +33,10 @@ $(function(){
     if (typeof moonwalk !== function(){}){
       clearInterval(moonwalk);
     }
-  }
+  };
+  
+  front.addEventListener("click", movef, false);
+  back.addEventListener("click", moveb, false);
+  pause.addEventListener("click", stopIt, false);
+  
 })();
