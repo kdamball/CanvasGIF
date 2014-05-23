8da3d069bc76599fd891b7229f23073997458b99
Kado Damball <kadodamball@hotmail.com>
2014-05-24 00:53:06 +0300

    Moving to requestAnimationFrame from setInterval

--- js/gifer.js
+++ js/gifer.js
@@ -8,32 +8,12 @@
     front = document.getElementById("front"),
     back = document.getElementById("back"),
     pause = document.getElementById("pause");
-  function moveb(){
-    stopIt();
-    moonwalk = setInterval(function(){
-      i ==-1?i=(x-1):i;
-      var pika = document.getElementById(pics[i]);
-      c.drawImage(pika,0,0);
-      i--;
-    },1000/8);
+
+  var moveFwd = function(){
+    var pika = document.getElementById(pics[i]);
+    c.drawImage(pika);
+    i++;
   }
-  function movef(){
-    stopIt();
-    forward = setInterval(function(){
-    i ==x?i=0:i;
-      var pika = document.getElementById(pics[i]);
-      c.drawImage(pika,0,0);
-      i++;
-    },1000/8);
-  }
-  function stopIt(){
-    if (typeof forward !== function(){}){
-    clearInterval(forward);
-    }
-    if (typeof moonwalk !== function(){}){
-      clearInterval(moonwalk);
-    }
-  };
   
   front.addEventListener("click", movef, false);
   back.addEventListener("click", moveb, false);
